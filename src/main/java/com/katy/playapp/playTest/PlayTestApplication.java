7db94ae9b45a16d9e40b47f2c50ee3054f057aa9
katydidnot <argykat@gmail.com>
2018-12-08 17:25:43 -0600

    initial

---
+++ src/main/java/com/katy/playapp/playTest/PlayTestApplication.java
@@ -0,0 +1,12 @@
+package com.katy.playapp.playTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayTestApplication.class, args);
+	}
+}
