c76f74f0f2c5b5ffa56ba9f893e35c98831bc998
katydidnot <argykat@gmail.com>
2018-12-08 18:05:17 -0600

    index/controller

---
+++ src/main/java/com/katy/playapp/playTest/controller/Controller.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.katy.playapp.playTest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author katyd
+ *
+ */
+
+@RestController
+@RequestMapping("/home")
+public class Controller {
+	
+	
+
+}
